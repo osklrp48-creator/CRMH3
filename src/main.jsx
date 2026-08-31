198840e7be2dba3bb5ccfc625eaf7d0d944b9445
osklrp48-creator <osklrp48@gmail.com>
2026-08-31 09:10:52 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
-// placeholder
+import React from "react";
+import ReactDOM from "react-dom/client";
+import H3CRM from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <H3CRM />
+  </React.StrictMode>
+);
